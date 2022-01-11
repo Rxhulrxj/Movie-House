d6720c2ad1585ca97adac44ae6a399307c4c3c27
RahulRajN <rahulrajachu14102000@gmail.com>
2022-01-11 19:23:28 +0530

    improved performance

---
+++ Components/Body/MovieCardlazyloaded.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+function Moviecards({ movie }) {
+  const router = useRouter();
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+  return (
+    <div
+      className="flex min-w-[180px] min-h-[270px] md:min-w-[220px] md:min-h-[340px] rounded-lg overflow-hidden shadow-xl cursor-pointer 
+      border-[3px] border-[#f9f9f9] border-opacity-10  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 ml-7 
+      relative w-[300px] h-[170px] group "
+      onClick={() => router.push(`/detail/${movie.id}`)}
+    >
+      <Image
+        src={`${BASE_URL}${movie.poster_path}`}
+        layout="fill"
+        objectFit="contain"
+        className="rounded-xl shadow-lg"
+        alt={movie?.title || movie?.original_title}
+      />
+      <div className="hidden group-hover:block group-hover:absolute group-hover:mt-[225px] group-hover:ml-0 group-hover:bg-zinc-900 w-[100%]">
+        <h2 className="text-white font-extrabold text-xl">
+          {movie?.title || movie?.original_title}
+        </h2>
+        <br />
+        <p className="text-white">⭐{movie.vote_average}/10</p>
+      </div>
+    </div>
+  );
+}
+
+export default Moviecards;
--- pages/DashBoard.js
+++ pages/DashBoard.js
@@ -1,6 +1,11 @@
+import dynamic from "next/dynamic";
 import Banner from "../Components/Body/Banner";
 import MovieCard from "../Components/Body/MovieCard";
 import FooterSection from "../Components/Footersection/FooterSection";
+import useInView from "react-cool-inview";
+const MovieCardloaded = dynamic(() =>
+  import("../Components/Body/MovieCardlazyloaded")
+);
 
 export default function DashBoard({
   UpcomingMovies,
@@ -14,8 +19,11 @@ export default function DashBoard({
   horror,
 }) {
   const banner = trendingdaily;
+  const { observe, inView } = useInView({
+    onEnter: ({ unobserve }) => unobserve(),
+  });
   return (
-    <div className="font-ABeeZee ">
+    <div className="font-ABeeZee " ref={observe}>
       <Banner trending={banner} />
       <h2 className="text-4xl ml-7 mt-8">Now Playing</h2>
       <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2  min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
@@ -35,36 +43,57 @@ export default function DashBoard({
           <MovieCard movie={movie} key={index} />
         ))}
       </div>
-      <h2 className="text-4xl ml-7 mt-8 ">Top-Rated Movies</h2>
-      <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
-        {TopRatedMovies?.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </div>
-      <h2 className="text-4xl ml-7 mt-8 ">Action Movies</h2>
-      <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
-        {action?.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </div>
-      <h2 className="text-4xl ml-7 mt-8 ">Comedy Movies</h2>
-      <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
-        {comedy?.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </div>
-      <h2 className="text-4xl ml-7 mt-8 ">Romantic Movies</h2>
-      <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
-        {romance?.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </div>
-      <h2 className="text-4xl ml-7 mt-8 ">Horror Movies</h2>
-      <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
-        {horror?.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </div>
+      {inView && (
+        <div>
+          <h2 className="text-4xl ml-7 mt-8 ">Top-Rated Movies</h2>
+          <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
+            {TopRatedMovies?.map((movie, index) => (
+              <MovieCardloaded movie={movie} key={index} />
+            ))}
+          </div>
+        </div>
+      )}
+
+      {inView && (
+        <div>
+          <h2 className="text-4xl ml-7 mt-8 ">Action Movies</h2>
+          <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
+            {action?.map((movie, index) => (
+              <MovieCardloaded movie={movie} key={index} />
+            ))}
+          </div>
+        </div>
+      )}
+      {inView && (
+        <div>
+          <h2 className="text-4xl ml-7 mt-8 ">Comedy Movies</h2>
+          <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
+            {comedy?.map((movie, index) => (
+              <MovieCardloaded movie={movie} key={index} />
+            ))}
+          </div>
+        </div>
+      )}
+      {inView && (
+        <div>
+          <h2 className="text-4xl ml-7 mt-8 ">Romantic Movies</h2>
+          <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
+            {romance?.map((movie, index) => (
+              <MovieCardloaded movie={movie} key={index} />
+            ))}
+          </div>
+        </div>
+      )}
+      {inView && (
+        <div>
+          <h2 className="text-4xl ml-7 mt-8 ">Horror Movies</h2>
+          <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 min-w-[240vw] lg:min-w-[210vw] xl:min-w-[201vh]">
+            {horror?.map((movie, index) => (
+              <MovieCardloaded movie={movie} key={index} />
+            ))}
+          </div>
+        </div>
+      )}
       <FooterSection />
     </div>
   );
